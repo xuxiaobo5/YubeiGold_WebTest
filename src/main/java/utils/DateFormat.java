a165ba28a227e575decbd3ae24fc9fee96a1dd66
XUXIAOBO_COMPANY <234508021@qq.com>
2017-06-20 16:13:57 +0800

    Upadate DateFormat!!!

--- src/main/java/utils/DateFormat.java
+++ src/main/java/utils/DateFormat.java
@@ -8,29 +8,19 @@ public class DateFormat {
 
     final static LoggerControler log = LoggerControler.getLog(DateFormat.class);
 
-    final static String COMMON_DATE_FORMAT = "yyyy-MM-dd";
-
-    final static String SHORT_DATE_FORMAT = "yy-MM-dd HH:mm";
-
-    final static String SMALL_DATE_FORMAT = "MM-dd";
-
-    final static String ABC_ORDER_DATE_FORMAT = "yyyy/MM/dd";
-
-    final static String ABC_ORDER_TIME_FORMAT = "HH:mm:ss";
-
-    final static String LOTTERY_PRINTING_TIME_FORMAT = "HH_mm_ss";
-
-    final static String CHECK_LOG_FORMAT = "yyyyMMdd";
-
-    final static String TEN_PAY_DATE_STRING_FORMAT = "yyMMdd";
-
-    final static String REPORT_CSV_FORMAT = "yyyyMMdd_HHmmss";
-
     final static String ZH_DATE_FORMAT = "yyyy年MM月dd日 HH:mm:ss";
 
     final static String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-    final static String DATE_FORMAT = "yyyy-MM-dd HH-mm-ss";
+    final static String COMMON_DATE_FORMAT = "yyyy-MM-dd HH-mm-ss";
+
+    final static String CHECK_LOG_FORMAT = "yyyyMMdd";
+
+    final static String ABC_DATE_FORMAT = "yyyy/MM/dd";
+
+    final static String ABC_TIME_FORMAT = "HH:mm:ss";
+
+    final static String LOTTERY_PRINTING_TIME_FORMAT = "HH_mm_ss";
 
     //引入SimpleDateFormat类处理时间
     private static SimpleDateFormat simpleDateFormat;
