915b916ae8ba4f21b6541cf11fc0d15c6cbdbe61
XUXIAOBO_COMPANY <234508021@qq.com>
2017-06-20 15:39:54 +0800

    Change public static final to final static

--- src/main/java/utils/DateFormat.java
+++ src/main/java/utils/DateFormat.java
@@ -8,31 +8,31 @@ public class DateFormat {
 
     final static LoggerControler log = LoggerControler.getLog(DateFormat.class);
 
-    public static final String COMMON_DATE_FORMAT = "yyyy-MM-dd";
+    final static String COMMON_DATE_FORMAT = "yyyy-MM-dd";
 
-    public static final String SHORT_DATE_FORMAT = "yy-MM-dd HH:mm";
+    final static String SHORT_DATE_FORMAT = "yy-MM-dd HH:mm";
 
-    public static final String SMALL_DATE_FORMAT = "MM-dd";
+    final static String SMALL_DATE_FORMAT = "MM-dd";
 
-    public static final String ABC_ORDER_DATE_FORMAT = "yyyy/MM/dd";
+    final static String ABC_ORDER_DATE_FORMAT = "yyyy/MM/dd";
 
-    public static final String ABC_ORDER_TIME_FORMAT = "HH:mm:ss";
+    final static String ABC_ORDER_TIME_FORMAT = "HH:mm:ss";
 
-    public static final String LOTTERY_PRINTING_TIME_FORMAT = "HH_mm_ss";
+    final static String LOTTERY_PRINTING_TIME_FORMAT = "HH_mm_ss";
 
-    public static final String CHECK_LOG_FORMAT = "yyyyMMdd";
+    final static String CHECK_LOG_FORMAT = "yyyyMMdd";
 
-    public static final String TEN_PAY_DATE_STRING_FORMAT = "yyMMdd";
+    final static String TEN_PAY_DATE_STRING_FORMAT = "yyMMdd";
 
-    public static final String REPORT_CSV_FORMAT = "yyyyMMdd_HHmmss";
+    final static String REPORT_CSV_FORMAT = "yyyyMMdd_HHmmss";
 
-    public static final String ZH_DATE_FORMAT = "yyyy年MM月dd日 HH:mm:ss";
+    final static String ZH_DATE_FORMAT = "yyyy年MM月dd日 HH:mm:ss";
 
-    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    final static String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-    public static final String DATE_FORMAT = "yyyy-MM-dd HH-mm-ss";
+    final static String DATE_FORMAT = "yyyy-MM-dd HH-mm-ss";
 
-    //	引入SimpleDateFormat类处理时间
+    //引入SimpleDateFormat类处理时间
     private static SimpleDateFormat simpleDateFormat;
 
     public static String format(String type) {
